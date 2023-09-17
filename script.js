d70c1a3b87b7615a3e7c3bc9cb5c88e19266f2ff
unknown <yashdnikam14@gmail.com>
2023-09-17 12:17:48 +0530

    second commit

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const dogBtn = document.getElementById('dogBtn');
 const dogImg = document.getElementById('dogImg');
 
 const displayDog = () => {
+    //url to fetch the data from api
     fetch('https://dog.ceo/api/breeds/image/random')
+    //returns a promise
     .then(Response => Response.json())
+    //gets data into json format
     .then(json => 
         {
             dogImg.innerHTML=`<img src='${json.message}' style='width:100%'></img>`
@@ -11,6 +14,7 @@ const displayDog = () => {
         });
 }
 
+//onclick function to display the dog images
 dogBtn.onclick=()=>{
     displayDog();
 }
